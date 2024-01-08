cdf7490b3c8c188380adb32a706bbe029e48a042
Felipe Razzini <feliperazzini95@gmail.com>
2024-01-08 23:32:30 +0100

    list controller and list index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,28 @@
 require 'uri'
 require 'net/http'
 
-url = URI("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1")
+url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
 
 request = Net::HTTP::Get.new(url)
 request["accept"] = 'application/json'
+request["Authorization"] = MOVIE_KEY
 
 response = http.request(request)
-puts response.read_body
+movies = response.read_body
+
+puts 'Cleaning database...'
+Movie.destroy_all
+
+puts 'Creating movies...'
+movies['results'].each do |movie|
+  movie = Movie.create!(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w400#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+puts 'Finished!'
